b9aa1a18f3f7ed4f92418f786bb0daab64e1092b
AXO <77832984+AqsoLath@users.noreply.github.com>
2024-01-12 23:26:41 +0700

    benerin navbar

--- wp-content/themes/website-3vhs/header.php
+++ wp-content/themes/website-3vhs/header.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
     <nav>
-        <ul>
-            <li><a href="/ " class="beranda">Beranda</a></li>
-            <li><a href="/roaster" class="roster">Roaster</a></li>
-            <li><a href="/gallery" class="gallery">Gallery</a></li>
-            <li><a href="/kontak" class="kontak">Kontak</a></li>
-        </ul>
+        <div class="nav-container container">
+            <div class="nav-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-sevory.png ?>" alt="">
+            </div>
+            <div class="nav-links">
+                <a href="/ " class="beranda">Beranda</a>
+                <a href="/roaster" class="roster">Roaster</a>
+                <a href="/gallery" class="gallery">Gallery</a>
+                <a href="/kontak" class="kontak">Kontak</a>
+            </div>
+        </div>
     </nav>
-   
