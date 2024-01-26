a0e16b317e5371ace10a9f173592911c595d1d72
AXO <77832984+AqsoLath@users.noreply.github.com>
2024-01-26 23:59:31 +0700

    bikin hero

--- wp-content/themes/website-3vhs/index.php
+++ wp-content/themes/website-3vhs/index.php
@@ -1,23 +1,37 @@
 <?php get_header() ?>
 
-<!--====================-->
-<!--===== MAINHERO =====-->
-<!--====================-->
-<div class="mainhero">
-    <div class="container mainhero-container">
-        <img src="<?php echo get_template_directory_uri(); ?> /assets/images/sevory-mainhero.png" alt="sevory mainhero">
+<main>
+    <!--====================-->
+    <!--===== MAINHERO =====-->
+    <!--====================-->
+    <div class="mainhero">
+        <div class="bg-konten-box"></div>
+        <div class="konten container">
+            <div class="konten-box">
+                <h1>Sevory</h1>
+                <h2>Basketball Team</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod sint quam repellendus ducimus excepturi eum nihil qui eos. Impedit dignissimos, minus incidunt voluptatum soluta, magnam architecto dolores saepe est iure itaque excepturi animi enim corporis voluptate. Odit perspiciatis autem ratione, accusantium nam magni sapiente esse quidem! Minima, vero aliquid?</p>
+                <a href="our-team" class="button-orange">
+                    <div>
+                        Our New Team 
+                    </div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vector/arrow-right.svg" class="ms-3" alt="arrow-right">
+                </a>
+            </div>
+        </div>
+        <div class="gambar">
+
+        </div>
     </div>
-</div>
 
-<!--===================-->
-<!--===== ROSTERS =====-->
-<!--===================-->
-<div class="rosters">
-    <div class="header" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/rosters-outline.png);">
-        <h2>ROSTERS</h2>
+    <!--===================-->
+    <!--===== ROSTERS =====-->
+    <!--===================-->
+    <div class="rosters">
+        <div class="header" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/rosters-outline.png);">
+            <h2>ROSTERS</h2>
+        </div>
     </div>
-</div>
-
-
+</main>
 
 <?php get_footer() ?>
