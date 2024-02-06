79b203b26cba6a2ff410887d69ddc017f1007a31
AXO <77832984+AqsoLath@users.noreply.github.com>
2024-02-06 18:54:58 +0700

    home main hero dan rosters

--- wp-content/themes/website-3vhs/functions.php
+++ wp-content/themes/website-3vhs/functions.php
@@ -2,23 +2,24 @@
 
 <?php
 
-function aqso_register_styles(){
+function aqso_register_styles()
+{
 
     // ENQUE STYLE
-    wp_enqueue_style( "bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", "", "1.0", "all" );
-    wp_enqueue_style( "aqsoOwnCss", get_template_directory_uri() . "/style/style.css", "", "1.0", "all" );
+    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", "", "1.0", "all");
     wp_enqueue_style('swiper_css', "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css");
     wp_enqueue_style('aos_css', "https://unpkg.com/aos@2.3.1/dist/aos.css");
+    wp_enqueue_style("aqsoOwnCss", get_template_directory_uri() . "/style/style.css", "", "1.0", "all");
 
 
     // ENQUE SCRIPT
     // Karena di wordpress udah tertanem jquery maka kita hanya perlu manggil file-nya aja, gak perlu didownload file-nya
-    wp_enqueue_script( "jquery");
+    wp_enqueue_script("jquery");
+    wp_enqueue_script('swiper_js', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', array('jquery'));
+    wp_enqueue_script('aos_js', 'https://unpkg.com/aos@2.3.1/dist/aos.js', array(), '', true);
     wp_enqueue_script('script_js', get_template_directory_uri() . '/script/script.js', array('jquery'), rand(111, 9999), true);
-    wp_enqueue_script('aos_js', "https://unpkg.com/aos@2.3.1/dist/aos.js");
-
 }
 
-add_action( "wp_enqueue_scripts", "aqso_register_styles" );
+add_action("wp_enqueue_scripts", "aqso_register_styles");
 
 ?>
--- wp-content/themes/website-3vhs/script/script.js
+++ wp-content/themes/website-3vhs/script/script.js
@@ -1,20 +1,45 @@
 
+//===============
+//===== AOS =====
+//===============
+AOS.init();
 
-const urlLocation = location.pathname;
+//==================
+//===== SWIPER =====
+//==================
+var swiperPlayers = new Swiper(".mainhero .swiper-players", {
+    direction: "vertical",
+    loop: true,
+    speed: 2000,
+    autoplay: {
+        delay: 500
+    },
+    // sliderChange: function () {
+    //     console.log('awikwok');
+    // }
 
-console.log("urlLocation  :" ,urlLocation,);
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+});
+
+swiperPlayers.on("beforeTransitionStart", () => {
+    console.log('awikwok');
+})
 
 
-if(urlLocation == '/'){
-    const beranda = document.getElementsByClassName('beranda');
-    beranda[0].classList.add("activate");
-} else if ( urlLocation == '/roaster') {
-    const roaster = document.getElementsByClassName('roaster');
-    roaster[0].classList.add("activate");
-} else if ( urlLocation == '/gallery') {
-    const gallery = document.getElementsByClassName('gallery');
-    gallery[0].classList.add("activate");
-} else if ( urlLocation == '/contact') {
-    const contact = document.getElementsByClassName('kontak');
-    contact[0].classList.add("activate");
-} 
+// if (urlLocation == '/') {
+//     const beranda = document.getElementsByClassName('beranda');
+//     beranda[0].classList.add("activate");
+// } else if (urlLocation == '/roaster') {
+//     const roaster = document.getElementsByClassName('roaster');
+//     roaster[0].classList.add("activate");
+// } else if (urlLocation == '/gallery') {
+//     const gallery = document.getElementsByClassName('gallery');
+//     gallery[0].classList.add("activate");
+// } else if (urlLocation == '/contact') {
+//     const contact = document.getElementsByClassName('kontak');
+//     contact[0].classList.add("activate");
+// }
+
