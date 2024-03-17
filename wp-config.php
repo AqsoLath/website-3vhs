8140eab3023047dbc0dc8714ac3e66862052800d
AshabilTaharica <ashabilsyauqi@gmail.com>
2024-03-17 16:53:12 +0700

    mengerjakan integrasi backend ke frontend
    by ashabil

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'website-3vhs' );
+define( 'DB_NAME', 'website--3vhs' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
--- wp-content/themes/website-3vhs/page-roster-boys.php
+++ wp-content/themes/website-3vhs/page-roster-boys.php
@@ -21,6 +21,42 @@
     </div>
 
 
+
+
+
+    <?php
+    $args = array(
+        'post_type' => array('boys-page'),
+        'posts_per_page' => -1,
+        // 'orderby'   => 'menu_order', 
+        // 'order' => "ASC"
+    );
+
+    $wp_query = new WP_Query($args);
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!--===========================-->
     <!--===== ROSTERS PLAYERS =====-->
     <!--===========================-->
@@ -28,36 +64,47 @@
     <div class="rosters-players">
         <div class="container">
             <div class="players-wrapper">
+
+            <?php if ($wp_query->have_posts()) : ?>
+            <?php while ($wp_query->have_posts()) : $wp_query->the_post() ?>
+                    
                 <div class="item">
                     <div class="atas">
                         <div class="konten">
-                            <h4>PG</h4>
+                            <h4><?php echo get_field('posisi');?></h4>
                             <div class="detail">
                                 <div class="detail-item">
                                     <h5>Position</h5>
-                                    <h6>Point Guard</h6>
+                                    <h6><?php echo get_field('posisi');?></h6>
                                 </div>
                                 <div class="detail-item">
                                     <h5>Height</h5>
-                                    <h6>168 cm</h6>
+                                    <h6><?php echo get_field('tinggi_badan');?> cm</h6>
                                 </div>
                                 <div class="detail-item">
                                     <h5>Weight</h5>
-                                    <h6>65 kg</h6>
+                                    <h6><?php echo get_field('berat_badan');?> kg</h6>
                                 </div>
                                 <div class="detail-item">
                                     <h5>Vocational</h5>
-                                    <h6>TKJ</h6>
+                                    <h6><?php echo get_field('jurusan');?></h6>
                                 </div>
                             </div>
                         </div>
+                        <!-- <img src="<?php echo get_template_directory_uri(get_field('foto-pemain')); ?> alt="<?php get_field('nama_pemain')?>" class="foto-profil"> -->
+          
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/players-boys/kenny-1.png" alt="kenny 1" class="foto-profil">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-sevory.png" alt="logo sevory" class="logo-sevory">
                     </div>
                     <div class="bawah">
-                        <h3>Kenny <strong>Imanuel</strong></h3>
+                        <!-- <h3>Lebron <strong>Imanuel</strong></h3> -->
+                  
+                        <h3><?php echo get_field('nama_pemain');?></h3>
                     </div>
                 </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+
             </div>
         </div>
     </div>
